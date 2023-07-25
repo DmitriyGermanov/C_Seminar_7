08a9c84c87731819554ea4de8f27a248c496e91c
Dmitriy Germanov <seltis.gtn@gmail.com>
2023-07-25 21:01:18 +0300

    added first task

---
+++ task01/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Clear();
+int rows = GetNum("Введите количество строк: ");
+int columns = GetNum("Введите количество столбцов: ");
+
+int[,] matrix = GetArray(rows, columns);
+
+PrintArray(matrix);
+
+int GetNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int[,] GetArray(int row, int column)
+{
+    int[,] res = new int[row, column];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            res[i, j] = i + j;
+        }
+    }
+    return res;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
