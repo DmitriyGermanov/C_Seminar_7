2e1223f4b0d18306767a091a0be9fe0b2580bb13
Dmitriy Germanov <seltis.gtn@gmail.com>
2023-07-25 21:13:26 +0300

    Added third task

---
+++ task02/Program.cs
@@ -0,0 +1,58 @@
+﻿//  Задайте двумерный массив. Найдите элементы, у
+// которых оба индекса чётные, и замените эти элементы на их
+// квадраты.
+
+Console.Clear();
+int rows = GetNum("Введите количество строк: ");
+int columns = GetNum("Введите количество столбцов: ");
+
+int[,] matrix = GetArray(rows, columns);
+PrintArray(matrix);
+
+matrix = FindArray(matrix);
+PrintArray(matrix);
+
+int GetNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int[,] GetArray(int row, int column)
+{
+    int[,] res = new int[row, column];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            res[i, j] = i + j;
+        }
+    }
+    return res;
+}
+
+
+int[,] FindArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+                array[i, j] *= array[i, j];
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
